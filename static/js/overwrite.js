35ab4066fa35fc79aa701ef4533b2e99b8b3a8f6
Euan Campbell <euanalisdaircampbell@gmail.com>
2021-02-28 17:21:32 +0000

    url redirect

--- static/js/overwrite.js
+++ static/js/overwrite.js
@@ -1,10 +1,9 @@
 function goToSubreddit(){
     // Selecting the input element and get its value 
     var inputVal = document.getElementById("subredditSearch").value;
-    var baseURL = "https://reddit.euan.app/r/";
+    var baseURL = window.location.hostname.concat('/r/');
 
     var url = baseURL.concat(inputVal)
-    console.log(url)
     alert(url);
     window.location.href = url;
 }
